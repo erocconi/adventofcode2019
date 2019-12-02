9ae8b2df7945908cdcb0ee6fd9d0fef633af0f5f
Eric Rocconi <eric@rocconi.me>
2019-12-02 15:30:22 -0800

    Derp

--- Day2.py
+++ Day2.py
@@ -1,30 +1,24 @@
 
-
 def do_work(position):
     global data
     first = data[data[(position + 1)]]
     second = data[data[(position + 2)]]
     if data[position] == 1:
         result = first + second
-        #print("Iteration {}: {} + {} = {}. Stored at location {}".format(count,first, second, result, data[(position + 3)]))
     elif data[position] == 2:
         result = first * second
-        #print("Iteration {}: {} * {} = {}. Stored at location {}".format(count,first, second, result, data[(position + 3)]))
     else: 
-        print("Something went wrong. {}".format(data[position]))
+        print("Something went wrong. {}".format(position))
     data[data[(position + 3)]] = result
 
 with open('Day2-Input.txt', 'r') as input:
     data = [int(x) for x in input.read().split(',')]
     incomplete = True
-    locale = 0
-    count = 1
+    inst = 0
     while incomplete == True:
-        if data[locale] == 99:
-            print(data[0])
-            #print(data)
+        if data[inst] == 99:
+            print("Tyranny of the Rocket Equation: {}".format(data[0]))
             incomplete = False
         else:
-            do_work(locale)
-            locale += 4
-            count += 1
+            do_work(inst)
+            inst += 4
